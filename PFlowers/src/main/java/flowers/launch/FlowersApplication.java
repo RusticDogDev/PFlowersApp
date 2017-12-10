4ff4a12bcc7f05a91e30925cc7a851f43068ea5c
SPEEDO\target <daniel.b.coughlan@mycit.ie>
2017-12-10 19:26:02 +0000

    minor changes

--- PFlowers/src/main/java/flowers/launch/FlowersApplication.java
+++ PFlowers/src/main/java/flowers/launch/FlowersApplication.java
@@ -1,12 +1,12 @@
-package com.example.demo;
+package flowers.launch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class FlowersApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(FlowersApplication.class, args);
 	}
 }
